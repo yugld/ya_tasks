e891142a300b52fa23ab461afc3c72ed2ebd01d1
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-09-26 13:25:42 +0600

    add context binding task

---
+++ binding/index.js
@@ -0,0 +1,13 @@
+const obj = { x: 15 };
+
+function first() {
+   return this.x; // 15
+}
+
+function second() {
+    // return first.apply(obj);
+	// return first.bind(obj)(a);
+	// return ({...obj, ...first}).first()
+	//return first.call(obj);
+	return first.bind(obj)(); 
+}
