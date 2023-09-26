6b60f2f66cceaae403b0b86ca263bd7108009bcf
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-09-26 13:11:21 +0600

    add closure tsk

---
+++ closure/index.js
@@ -0,0 +1,12 @@
+const object = {
+    value: '42',
+    type() {
+        return typeof this.value;
+    },
+    print() {
+
+        console.log(`${this.value} is ${this.type()}`);
+    }
+}
+
+object.print(); // Поправьте замыкание
